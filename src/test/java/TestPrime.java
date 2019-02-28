d2bca034bc223cc3643e6040a98097e735344118
Roman Bulinski <aboandrus@gmail.com>
2019-02-28 07:09:04 +0100

    Add new test

---
+++ src/test/java/TestPrime.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPrime {
+
+    @Test
+    public void  testPrime(){
+        Prime prime = new Prime();
+        List<Integer> reuslList = Arrays.asList(2,3,5,7,11);
+        assertEquals(  reuslList, prime.getlistOfPrimeNumbers(5));
+    }
+
+    //2, 3, 5, 7, 11, 13, 17, 19, 23,
+
+    @Test
+    public void  testPrime8(){
+        Prime prime = new Prime();
+        List<Integer> reuslList = Arrays.asList( 2, 3, 5, 7, 11, 13, 17, 19, 23 );
+        assertEquals(  reuslList, prime.getlistOfPrimeNumbers(9));
+    }
+
+    @Test
+    public void  testPrime1(){
+        Prime prime = new Prime();
+        List<Integer> reuslList = Arrays.asList( 2 );
+        assertEquals(  reuslList, prime.getlistOfPrimeNumbers(1));
+    }
+
+
+
+
+}
